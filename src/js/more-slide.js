3d3f2b97979dd5fe7afa9609b1da3888e32b4669
Carie Fisher <carie.fisher@mediacurrent.com>
2017-03-19 21:37:12 -0500

    styling for open/close toggle

--- src/js/more-slide.js
+++ src/js/more-slide.js
@@ -1,7 +1,14 @@
 !((document, $) => {
   'use strict';
 
-  $('.more').on('click', function() {
-    $('.more-info').toggleClass('more-info-open');
+  // Toggle on click.
+  $('.more-toggle').on('click', function() {
+
+    var isActive = ($(this).hasClass('more-open')) ? true : false; // checks if it is already active
+      $('.more-toggle').removeClass('more-open');
+      if (!isActive) $(this).addClass('more-open'); // set active only if it was not active
+
+    // Cancel the click event.
+    return false;
   });
 })(document, jQuery);
