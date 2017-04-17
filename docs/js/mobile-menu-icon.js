6439189c2f815c5816f19e9025fc6228c5fe486d
Carie Fisher <carie.fisher@mediacurrent.com>
2017-04-16 19:09:47 -0500

    demo update

--- docs/js/mobile-menu-icon.js
+++ docs/js/mobile-menu-icon.js
@@ -1,6 +1,6 @@
 !((document, $) => {
-  // Set button to click.
-  var button = document.getElementById( 'menu-toggle' );
+  // Set button to click. Toggle button example 1.
+  var button = document.getElementById( 'menu-toggle-ex1' );
 
   // Click the button.
   button.onclick = function() {
@@ -12,6 +12,40 @@
     } else {
        button.className += ' opened';
       button.setAttribute( 'aria-expanded', 'true' );
+     }
+
+   };
+
+   // Set button to click. Toggle button example 2.
+  var button2 = document.getElementById( 'menu-toggle-ex2' );
+
+  // Click the button.
+  button2.onclick = function() {
+
+    // Toggle class "opened". Set also aria-expanded to true or false.
+    if ( -1 !== button2.className.indexOf( 'opened' ) ) {
+      button2.className = button2.className.replace( ' opened', '' );
+      button2.setAttribute( 'aria-expanded', 'false' );
+    } else {
+       button2.className += ' opened';
+      button2.setAttribute( 'aria-expanded', 'true' );
+     }
+
+   };
+
+   // Set button to click. Mobile menu button.
+  var buttonmm = document.getElementById( 'toggle-menu' );
+
+  // Click the button.
+  buttonmm.onclick = function() {
+
+    // Toggle class "opened". Set also aria-expanded to true or false.
+    if ( -1 !== buttonmm.className.indexOf( 'opened' ) ) {
+      buttonmm.className = buttonmm.className.replace( ' opened', '' );
+      buttonmm.setAttribute( 'aria-expanded', 'false' );
+    } else {
+       buttonmm.className += ' opened';
+      buttonmm.setAttribute( 'aria-expanded', 'true' );
      }
 
    };
