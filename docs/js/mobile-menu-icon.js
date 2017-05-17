739e0dd845ffa9eec84dbcd12eb450dbfa1401a5
Carie Fisher <carie.fisher@mediacurrent.com>
2017-05-16 22:39:01 -0500

    demo updates

--- docs/js/mobile-menu-icon.js
+++ docs/js/mobile-menu-icon.js
@@ -1,52 +1,54 @@
-!((document, $) => {
-  // Set button to click. Toggle button example 1.
-  var button = document.getElementById( 'menu-toggle-ex1' );
+window.onload = function(){
+  !((document, $) => {
+    // Set button to click. Toggle button example 1.
+    var button = document.getElementById( 'menu-toggle-ex1' );
 
-  // Click the button.
-  button.onclick = function() {
+    // Click the button.
+    button.onclick = function() {
 
-    // Toggle class "opened". Set also aria-expanded to true or false.
-    if ( -1 !== button.className.indexOf( 'opened' ) ) {
-      button.className = button.className.replace( ' opened', '' );
-      button.setAttribute( 'aria-expanded', 'false' );
-    } else {
-       button.className += ' opened';
-      button.setAttribute( 'aria-expanded', 'true' );
-     }
+      // Toggle class "opened". Set also aria-expanded to true or false.
+      if ( -1 !== button.className.indexOf( 'opened' ) ) {
+        button.className = button.className.replace( ' opened', '' );
+        button.setAttribute( 'aria-expanded', 'false' );
+      } else {
+         button.className += ' opened';
+        button.setAttribute( 'aria-expanded', 'true' );
+       }
 
-   };
+     };
 
-   // Set button to click. Toggle button example 2.
-  var button2 = document.getElementById( 'menu-toggle-ex2' );
+     // Set button to click. Toggle button example 2.
+    var button2 = document.getElementById( 'menu-toggle-ex2' );
 
-  // Click the button.
-  button2.onclick = function() {
+    // Click the button.
+    button2.onclick = function() {
 
-    // Toggle class "opened". Set also aria-expanded to true or false.
-    if ( -1 !== button2.className.indexOf( 'opened' ) ) {
-      button2.className = button2.className.replace( ' opened', '' );
-      button2.setAttribute( 'aria-expanded', 'false' );
-    } else {
-       button2.className += ' opened';
-      button2.setAttribute( 'aria-expanded', 'true' );
-     }
+      // Toggle class "opened". Set also aria-expanded to true or false.
+      if ( -1 !== button2.className.indexOf( 'opened' ) ) {
+        button2.className = button2.className.replace( ' opened', '' );
+        button2.setAttribute( 'aria-expanded', 'false' );
+      } else {
+         button2.className += ' opened';
+        button2.setAttribute( 'aria-expanded', 'true' );
+       }
 
-   };
+     };
 
-   // Set button to click. Mobile menu button.
-  var buttonmm = document.getElementById( 'toggle-menu' );
+     // Set button to click. Mobile menu button.
+    var buttonmm = document.getElementById( 'toggle-menu' );
 
-  // Click the button.
-  buttonmm.onclick = function() {
+    // Click the button.
+    buttonmm.onclick = function() {
 
-    // Toggle class "opened". Set also aria-expanded to true or false.
-    if ( -1 !== buttonmm.className.indexOf( 'opened' ) ) {
-      buttonmm.className = buttonmm.className.replace( ' opened', '' );
-      buttonmm.setAttribute( 'aria-expanded', 'false' );
-    } else {
-       buttonmm.className += ' opened';
-      buttonmm.setAttribute( 'aria-expanded', 'true' );
-     }
+      // Toggle class "opened". Set also aria-expanded to true or false.
+      if ( -1 !== buttonmm.className.indexOf( 'opened' ) ) {
+        buttonmm.className = buttonmm.className.replace( ' opened', '' );
+        buttonmm.setAttribute( 'aria-expanded', 'false' );
+      } else {
+         buttonmm.className += ' opened';
+        buttonmm.setAttribute( 'aria-expanded', 'true' );
+       }
 
-   };
-})(document, jQuery);
+     };
+  })(document, jQuery);
+}
