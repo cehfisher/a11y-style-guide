07a9e15a804a2072f27ab4a7885e30687507ee88
Mario Hernandez <mario.hernandez@mediacurrent.com>
2017-05-29 18:37:52 -0700

    Adds component to demo state
    Instead using a .is-disabled class, a more accessible way is to use
    aria attribute and listen for when a button (in this example), is pressed
    to disable it.

---
+++ src/components/state/state.es6.js
@@ -0,0 +1,9 @@
+!((document, $) => {
+  var pressedBtn = document.getElementById('press_me');
+
+  pressedBtn.addEventListener('click', function ( e ) {
+    /* toggle the pressed state of the button based on
+       whatever the current value is */
+    pressedBtn.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
+  });
+})(document, jQuery);
