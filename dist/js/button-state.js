424d72af1eda146568ac67ee678d2589a830acd3
Carie Fisher <carie.fisher@mediacurrent.com>
2017-05-30 22:58:44 -0500

    JS update for multiple button examples

--- dist/js/button-state.js
+++ dist/js/button-state.js
@@ -5,4 +5,15 @@
     pressedBtn.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
   });
 
+  var pressedBtn2 = document.getElementById('button_all');
+
+  pressedBtn2.addEventListener('click', function (e) {
+    pressedBtn2.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
+  });
+
+  var pressedBtn3 = document.getElementById('button_search');
+
+  pressedBtn3.addEventListener('click', function (e) {
+    pressedBtn3.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
+  });
 })(document, jQuery);
