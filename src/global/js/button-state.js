4bc1ddcac93b26c41ce720eaff22ce93d93b8ab2
Carie Fisher <carie.fisher@mediacurrent.com>
2017-05-30 23:29:12 -0500

    button component updates

--- src/global/js/button-state.js
+++ src/global/js/button-state.js
@@ -5,15 +5,21 @@
     pressedBtn.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
   });
 
-  var pressedBtn2 = document.getElementById('button_all');
+  var pressedBtn2 = document.getElementById('button_like');
 
   pressedBtn2.addEventListener('click', function (e) {
     pressedBtn2.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
   });
 
-  var pressedBtn3 = document.getElementById('button_search');
+  var pressedBtn3 = document.getElementById('button_all');
 
   pressedBtn3.addEventListener('click', function (e) {
     pressedBtn3.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
+  });
+
+  var pressedBtn4 = document.getElementById('button_search');
+
+  pressedBtn4.addEventListener('click', function (e) {
+    pressedBtn4.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
   });
 })(document, jQuery);
