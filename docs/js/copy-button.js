e373f96aea4ecd4b86889056ff8b145f3374e67b
cehfisher <cehfisher@gmail.com>
2018-01-28 15:17:35 -0600

    name change

--- docs/js/copy-button.js
+++
@@ -1,8 +0,0 @@
-!((document, $) => {
-  var clip = new Clipboard('.copy-button');
-
-  clip.on('success', function(e) {
-    $('.copied').show();
-    $('.copied').fadeOut(2000);
-  });
-})(document, jQuery);
