d8d5c9004bbcd038ebdea902c1cad56a0b6153b6
Carie Fisher <carie.fisher@deque.com>
2019-05-16 21:49:57 -0500

    updates to docs

--- docs/js/mobile-menu.js
+++ docs/js/mobile-menu.js
@@ -1,6 +1,24 @@
-$(function() {
+!((document, $) => {
   $('#toggle-menu').click(function(e) {
     e.preventDefault();
     $('ul').toggleClass('active');
   });
-});
+
+
+   // Set button to click. Mobile menu button.
+  var buttonmm = document.getElementById( 'toggle-menu' );
+
+  // Click the button.
+  buttonmm.onclick = function() {
+
+    // Toggle class "opened". Set also aria-expanded to true or false.
+    if ( -1 !== buttonmm.className.indexOf( 'opened' ) ) {
+      buttonmm.className = buttonmm.className.replace( ' opened', '' );
+      buttonmm.setAttribute( 'aria-expanded', 'false' );
+    } else {
+       buttonmm.className += ' opened';
+      buttonmm.setAttribute( 'aria-expanded', 'true' );
+     }
+
+   };
+})(document, jQuery);
